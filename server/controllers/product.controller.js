02b820d82f40e333af2b784b67b9b101e8e635fc
Arina <petrinhtrinh182@gmail.com>
2021-05-08 01:32:35 +0700

    fix react docker

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -67,7 +67,7 @@ const getProductCategory = async (req, res) => {
 		const page = parseInt(req.query.page) || 1;
 		const limit = parseInt(req.query.limit) || 10;
 		const startIndex = (page - 1) * limit;
-
+63
 		const productFeatures = new ProductFeatures(
 			Products.find({ category }),
 			req.query,
