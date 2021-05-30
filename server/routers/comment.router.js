a4ffba340f1e8b244b27492dff3e0e356920896c
minhnhatkool123 <bokool789@gmail.com>
2021-05-30 23:46:22 +0700

    add-jwt-comment

--- server/routers/comment.router.js
+++ server/routers/comment.router.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const commentController = require('../controllers/comment.controller');
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAmin');
 
-router.post('/add', commentController.addComment);
+router.post('/add', auth, commentController.addComment);
 
 router.get('/get', commentController.getComment);
 
-router.delete('/delete', commentController.deleteComment);
+router.delete('/delete', auth, authAdmin, commentController.deleteComment);
 
 module.exports = router;
